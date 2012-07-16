2f935dc58ef4ba5b9635d572797ee37e7e2f694b
Wakaba <wakaba@lemon>
2012-07-16 09:23:48 +0900

    git log json converter

---
+++ bin/gitlog2json.pl
@@ -0,0 +1,29 @@
+#!/usr/bin/perl
+use strict;
+BEGIN {
+  my $file_name = __FILE__; $file_name =~ s{[^/]+$}{}; $file_name ||= '.';
+  $file_name .= '/../config/perl/libs.txt';
+  if (-f $file_name) {
+    open my $file, '<', $file_name or die "$0: $file_name: $!";
+    unshift @INC, split /:/, scalar <$file>;
+  }
+}
+use warnings;
+use Path::Class;
+use lib file (__FILE__)->dir->parent->subdir ('lib')->stringify;
+use lib glob file (__FILE__)->dir->parent->subdir ('modules', '*', 'lib')->stringify;
+use Git::Parser::Log;
+use JSON::Functions::XS qw(perl2json_bytes_for_record);
+
+local $/ = undef;
+my $parsed = Git::Parser::Log->parse_format_raw (<>);
+print perl2json_bytes_for_record $parsed;
+
+=head1 LICENSE
+
+Copyright 2012 Wakaba <w@suika.fam.cx>.
+
+This program is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
---
+++ t/gitlog2json.t
@@ -0,0 +1,20 @@
+#!/bin/sh
+echo "1..1"
+rootdir=`dirname $0`/..
+tempfile=`perl -MFile::Temp -e 'print File::Temp->new->filename'`
+
+perl $rootdir/bin/gitlog2json.pl < $rootdir/t/data/git-log-formatraw.txt > $tempfile
+if diff -u $tempfile $rootdir/t/data/git-log-formatraw.json -q; then
+  echo "ok 1"
+else
+  echo "not ok 1"
+fi
+
+rm $tempfile
+
+# LICENSE
+# 
+# Copyright 2012 Wakaba <w@suika.fam.cx>.
+# 
+# This program is free software; you can redistribute it and/or modify
+# it under the same terms as Perl itself.
